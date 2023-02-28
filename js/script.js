d75263d321567c817b0c87fda5d4f89558bc6b2f
mintusikder <mintu141617@gmail.com>
2023-02-28 22:47:37 +0600

    added player info

--- js/script.js
+++ js/script.js
@@ -7,8 +7,34 @@ const searchAllData = () =>{
     console.log(URl)
 
 
-    // false(URl)
-    // .then((res)=> res.json())
-    // .then(data => console.log(data))
+    fetch(URl)
+    .then((res)=> res.json())
+    .then(data => showPlayerData(data.player))
+}
+const showPlayerData =(players)=>{
+    // console.log(players)
+    const container =document.getElementById('player-info')
+    container.innerHTML =' '
+    players.forEach((player) => {
+        const {strThumb, strPlayer,strNationality} = player
+        console.log(player)
+        const div =document.createElement('div')
+        div.classList.add("col")
+        div.innerHTML=`
+        <div class="card">
+          <img src="${strThumb ? strThumb : 'https://picsum.photos/id/237/200/300'}" class="card-img-top" alt="...">
+          <div class="card-body">
+        <h5 class="card-title">${strPlayer}</h5>
+            <p class="card-text">${strNationality}</p>
+        <div>
+        <button type="button" class="btn btn-success">Details</button>
+        <button type="button" class="btn btn-success">Info</button>
+        </div>
+          </div>
+        </div> `
+        container.appendChild(div)
+    });
+
 }
 // searchAllData()
+
